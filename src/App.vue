707758f037ae13fbc752c742fcb2430089d8ee35
Trysten M <nick.tristen22@gmail.com>
2024-01-21 12:02:00 +0200

    update (app)

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+import TheMain from "./components/TheMain.vue";
+</script>
+
+<template>
+  <TheMain/>
+</template>
+
+<style lang="scss">
+@import "assets/css/templates/main/variables";
+#app {
+  background: $colorMainDark;
+}
+</style>

93a96ab7cd0e365b51b13dc872bb97fe7ec7865d
Trysten M <nick.tristen22@gmail.com>
2024-01-21 11:59:19 +0200

    add (assets): add icons

---
+++ src/helpers/icons.js
@@ -0,0 +1,8 @@
+export const iconsPath = {
+    SHOW: '/src/assets/icons/show-svgrepo-com.svg',
+    HIDE: '/src/assets/icons/hide-svgrepo-com.svg',
+    ADD: '/src/assets/icons/addition-round-svgrepo-com.svg',
+    REMOVE: '/src/assets/icons/x-round-svgrepo-com.svg',
+    DOWNLOAD: '/src/assets/icons/download-svgrepo-com.svg',
+    UPLOAD: '/src/assets/icons/upload-svgrepo-com.svg',
+}

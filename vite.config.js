4f85bba3460352b56208679796f9a26e7e914123
Trysten M <nick.tristen22@gmail.com>
2024-01-23 23:59:32 +0200

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,21 @@ import vue from '@vitejs/plugin-vue'
 import {fileURLToPath, URL} from "node:url";
 import autoprefixer from "autoprefixer";
 
-export default defineConfig({
-  plugins: [vue()],
-  css: {
-    postcss: {
-      plugins: [autoprefixer],
+export default defineConfig(({mode, command}) => {
+    const base = command === 'build' ? '/simple-cv-generator/' : '/'
+
+    return {
+        plugins: [vue()],
+        css: {
+            postcss: {
+                plugins: [autoprefixer],
+            }
+        },
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url))
+            }
+        },
+        base
     }
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
 })
